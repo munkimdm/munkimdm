2730e5252ad0e2c73895a09dc69ce95244b47abe
Rod Christiansen <rodchristiansen@me.com>
2020-09-21 12:52:54 -0700

    Credentials for MicroMDM and VPP apps

---
+++ env.py
@@ -0,0 +1,11 @@
+settings = {
+    'micromdm_url': 'https://micromdm.domain.com',
+    'micromdm_key': 'SUPER_SECRET_API_KEY',
+    'basic_auth_user': 'MIDDLEWARE_USERNAME_IN_MUNKI_PKGINFO',
+    'basic_auth_password': 'MIDDLEWARE_PASSWORD_IN_MUNKI_PKGINFO',
+    'configurl': 'https://vpp.itunes.apple.com/mdm/VPPClientConfigSrv',
+    'licensesurl': 'https://vpp.itunes.apple.com/mdm/getVPPLicensesSrv',
+    'assetsurl': 'https://vpp.itunes.apple.com/mdm/getVPPAssetsSrv',
+    'manageurl': 'https://vpp.itunes.apple.com/mdm/manageVPPLicensesByAdamIdSrv',
+    'sToken':'YOUR_VPP_TOKEN_FROM_ABM_OR_ASM'
+}
