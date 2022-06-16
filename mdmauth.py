57af32995410ae13d7ac3751d49695aad5c0e1c4
Rod Christiansen <rodchristiansen@me.com>
2022-06-16 16:07:19 -0700

    Adds `DisassociateApplication` to middleware flask app

--- mdmauth.py
+++ mdmauth.py
@@ -10,7 +10,7 @@ app.config['BASIC_AUTH_USERNAME'] = settings.get('basic_auth_user')
 app.config['BASIC_AUTH_PASSWORD'] = settings.get('basic_auth_password')
 basic_auth = BasicAuth(app)
 
-supported_commands = ['InstallApplication', 'InstallEnterpriseApplication', 'InstallProfile', 'RemoveProfile', 'EnableRemoteDesktop', 'DisableRemoteDesktop', 'SetFirmwarePassword', 'VerifyFirmwarePassword', 'RestartDevice', 'ShutDownDevice']
+supported_commands = ['InstallApplication', 'DisassociateApplication', 'InstallEnterpriseApplication', 'InstallProfile', 'RemoveProfile', 'EnableRemoteDesktop', 'DisableRemoteDesktop', 'RestartDevice', 'ShutDownDevice', 'EraseDevice' , 'ScheduleOSUpdate']
 
 @app.route('/api/<command>', methods=['GET', 'POST'])
 @basic_auth.required
@@ -28,16 +28,13 @@ def api(command):
         'request_type': command
     }
     check('udid')
-    check('currentpassword')            # For SetFirmwarePassword
-    check('newpassword')                # For SetFirmwarePassword
-    check('password')                   # For VerifyFirmwarePassword
     check('pin')                        # For DeviceLock
     check('product_key')                # For ScheduleOSUpdate
     check('install_action')             # For ScheduleOSUpdateScan
     check('force')                      # For ScheduleOSUpdateScan
-    check('payload')	                # For InstallProfile
+    check('payload')	                 # For InstallProfile
     check('identifier')                 # For RemoveProfile
-    check('manifest_url')	            # For InstallEnterpriseApplication
+    check('manifest_url')	             # For InstallEnterpriseApplication
     check('serial')                     # For InstallVPPApplication
     check_int('itunes_store_id')        # For InstallVPPApplication
     if 'InstallApplication' in command:
@@ -54,10 +51,33 @@ def api(command):
         print(data)
         if get_occurrence_of_value(data, value=content['itunes_store_id']) == 0:
             # Assign this to this serial number
-            print("Not assigned to this serial, doing so now")
+            print("Sending InstallApplication for VPP app")
             params = dict(
                 sToken=settings.get('sToken'),
                 associateSerialNumbers=[content['serial']],
+                pricingParam="STDQ",
+                adamIdStr=content['itunes_store_id']
+                )
+            print(requests.post(url=settings.get('manageurl'), json=params))
+            print(params)
+    if 'DisassociateApplication' in command:
+        options = {}
+        options['purchase_method'] = int(1)
+        payload['options'] = options
+        # Get List of Licenses associated with Serial
+        params = dict(
+            sToken=settings.get('sToken'),
+            serialNumber=content['serial']
+        )
+        resp = requests.get(url=settings.get('licensesurl'), json=params)
+        data = resp.json()
+        print(data)
+        if get_occurrence_of_value(data, value=content['itunes_store_id']) == 0:
+            # Assign this to this serial number
+            print("Sending InstallApplication for VPP app")
+            params = dict(
+                sToken=settings.get('sToken'),
+                disassociateSerialNumbers=[content['serial']],
                 pricingParam="STDQ",
                 adamIdStr=content['itunes_store_id']
                 )
